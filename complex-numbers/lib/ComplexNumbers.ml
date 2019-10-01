1a4e8b3fe6ef7950d9974712d70b28422dbff49f
jcc8 <jcc8@hw.ac.uk>
2019-10-01 20:32:49 +0100

    jcc8; ComplexNumbers.ml; lines 3, 4, 5, 10, 13 and 14; 1/10/2019 20:32; writing cadd and cmul functions

--- complex-numbers/lib/ComplexNumbers.ml
+++ complex-numbers/lib/ComplexNumbers.ml
@@ -1,12 +1,15 @@
 type complex_number = CI of int*int;;
 
+(* auxiliar functions *)
+(* multiplies two integers *)
+let mInt = fun x y -> x * y ;;
+
 (* implementations *)
 
 (* addition of two Complex Integers *)
-let cadd (CI(x,y)) (CI(x2,y2)) =
-  failwith "not implemented yet" ;;
+let cadd (CI(x,y)) (CI(x2,y2)) = CI( ( x + x2), ( y + y2)) ;;
 
 (* multiplication of two Complex Integers *)
 let cmult (CI(x,y)) (CI(x2,y2)) =
-  failwith "not implemented yet" ;;
+  CI( ( (mInt x x2) - (mInt y y2) ), ( (mInt x y2) + (mInt y x2)) ) ;;
 
