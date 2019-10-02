44a7e109f90f689658a3e1e59ee13f7e0b1c698c
jcc8 <jcc8@hw.ac.uk>
2019-10-02 18:35:15 +0100

    jcc8; SequenceArithmetic.ml; lines 6, 7, 13 to 17 and 23 to 25; 2/10/2019 18:34; set seqadd and seqmult function bodys, declare auxiliar function to multiply integers

--- sequence-arithmetic/lib/SequenceArithmetic.ml
+++ sequence-arithmetic/lib/SequenceArithmetic.ml
@@ -3,17 +3,23 @@
 
 type intseq = int list;;
 
+let mInt = fun x y -> x * y ;;
+
 (* implementations *)
 
 (* function that adds two sequences from head to tail, summing at each
    position. *)
 let rec seqadd : intseq -> intseq -> intseq =
-  fun xs ys ->
-  failwith "not implemented yet" ;;
+  fun xs ys -> match xs, ys with
+    [x], [y] -> [( x + y )]
+    | h1::rest1, h2::rest2 -> ( h1 + h2 ) :: seqadd rest1 rest2
+    | xs, [] -> []
+    | [], ys -> [] ;;
 
   
 (* function that multiplies two lists from head to tail, multiplying
    at each position *)
 let rec seqmult : intseq -> intseq -> intseq =
-  fun xs ys ->
-  failwith "not implemented yet" ;;
+  fun xs ys -> match xs, ys with
+    [x], [y] -> [( mInt x y )]
+    | h1::rest1, h2::rest2 -> ( mInt h1 h2) :: seqmult rest1 rest2 ;;
