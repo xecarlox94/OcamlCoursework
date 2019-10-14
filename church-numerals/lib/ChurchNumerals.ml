8c61d41b431f5ac4214a15c5e44d2b38592cd13b
jcc8 <jcc8@hw.ac.uk>
2019-10-14 16:53:30 +0100

    church numerals i2c recursive function

--- church-numerals/lib/ChurchNumerals.ml
+++ church-numerals/lib/ChurchNumerals.ml
@@ -1,7 +1,9 @@
 
 (* i2c recursively applies a function f to a parameter x n times *)
 let rec i2c i f x =
-  failwith "not implemented yet" ;;
+  match i with
+    0 -> x
+    | i -> i2c (i - 1) f (f x) ;;
 
 (* passes the incrementing function and 0 into the Church numeral, so
    every application of f increases the output by 1. Since it starts
