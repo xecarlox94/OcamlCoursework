51558ba6578f6bb6dfac63e65ac8995d0b28d436
jcc8 <jcc8@hw.ac.uk>
2019-10-24 17:19:44 +0100

    testing for mult_int function

--- ocaml-essay/test/EssayTests.ml
+++ ocaml-essay/test/EssayTests.ml
@@ -5,14 +5,25 @@ open OUnit2;;
 (* unit tests *)
 
 let add_test1 _test_ctxt =
-  assert_equal 9 (add 5 4);;
+  assert_equal 9 (add 5 4) ;;
+
+(* it tests if 3 times 2 is equal to 6 *)
+let mul_int_utest _text_ctxt =
+  assert_equal 6 (mul_int 3 2) ;;
 
 (* list of unit tests *)
 let unit_tests =
   [ "add_test1">::add_test1
+  ; "mul_int_utest">::mul_int_utest
   ];;
 
 (* property based tests *)
+
+(* check if a random number is multiplied by zero *)
+let mul_int_ptest =
+  QCheck.Test.make ~name:"mul_int" ~count:1000
+    QCheck.(make Gen.nat )
+      (fun x -> mul_int x 0 = 0 ) ;;
 
 let add_zero =
   QCheck.Test.make ~name:"seqmult_zeros" ~count:1000
@@ -25,6 +36,7 @@ let add_zero =
 let property_tests =
   List.map QCheck_ounit.to_ounit2_test
     [ add_zero
+    ; mul_int_ptest
     ];;
 
 (* run the unit and property based tests *)
